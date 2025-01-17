6f48e060793524ac5cbfb5f2598f6ee1bb186ee3
Nayyer Alam <mdnayyer46@gmail.com>
2025-01-17 12:16:33 +0530

    handled respnsiveness of whole page

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -1,30 +1,7 @@
-// import React from "react";
-// import "./navbar.css";
-// import logo from "../../assets/logo.png"
-
-// const Navbar = () => {
-//   return (
-//     <nav className="navbar">
-//       <ul className="nav-links">
-//         <span><li>Home</li></span>
-        
-//         <li>About Us</li>
-//         <li>Features</li>
-//         <li>FAQ</li>
-//         <li>Success Stories</li>
-//         <li>Development</li>
-//       </ul>
-//       <img className="nav-img" src={logo} alt="" />
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import React, { useState } from "react";
 import "./navbar.css";
 import logo from "../../assets/logo.png";
-// import hamburger from "../../assets/hamburger.png";
+import hamburger from "../../assets/hamburger.png";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -35,19 +12,21 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <img className="nav-img" src={logo} alt="Logo" />
-      <div className="hamburger" onClick={toggleMenu}>
-        {/* Hamburger icon */}
-        {/* <img src={hamburger} alt="Logo" /> */}
-      </div>
-      <ul className={`nav-links ${isMenuOpen ? "active" : ""}`} >
-        <li>Home</li>
+      
+      <ul className={`nav-links ${isMenuOpen ? "active" : ""}`}>
+      <span><li>Home</li></span>
         <li>About Us</li>
         <li>Features</li>
         <li>FAQ</li>
         <li>Success Stories</li>
         <li>Development</li>
       </ul>
+
+      <img className="nav-img" src={logo} alt="Logo" />
+      <div className="hamburger" onClick={toggleMenu}>
+        {/* Hamburger icon */}
+        <img src={hamburger} alt="Logo" />
+      </div>
     </nav>
   );
 };
